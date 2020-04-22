d46982dd48a39168ea434f5e8fa747f7859fa851
EliAlkorta <elialkor@gmail.com>
2020-04-22 16:59:35 +0200

    initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../Stylesheets/App.scss';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+       
+      </div>
+    );
+  }
+}
+
+export default App;
